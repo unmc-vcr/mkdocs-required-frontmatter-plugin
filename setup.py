bf5fb2a4824ed288be96b9bdac3908be67426c50
James Geiger <james@lavande.in>
2024-03-21 16:49:57 -0500

    - update setup.py with additional details - add gitignore

--- setup.py
+++ setup.py
@@ -3,6 +3,22 @@ from setuptools import find_packages, setup
 setup(
     name="mkdocs-required-tags-plugin",
     packages=find_packages(),
+    version="0.0.2",
+    keywords="mkdocs plugin tags required",
+    url="https://github.com/unmc-vcr/mkdocs-required-tags-plugin",
+    author="James Geiger",
+    author_email="james.geiger@unmc.edu",
+    license="MIT",
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         "mkdocs.plugins": [
             "required-tags = src.plugin:RequiredTagsPlugin"
